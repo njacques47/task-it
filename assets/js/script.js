bb0bd009ac06434184497e36ae0f7e1cbc2b3cf9
njacques47 <nojacques94@gmail.com>
2022-02-06 23:35:19 -0500

    form submission handling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    // prevents page refresh on submit
+    event.preventDefault();
     // create a new list item when the button is clicked
     var listItemEl = document.createElement("li");
     // apply a class name to the newly created li to keep styling
@@ -10,7 +12,9 @@ var createTaskHandler = function() {
     listItemEl.textContent = "This is a new task.";
     // attach the li to the DOM in browser
     tasksToDoEl.appendChild(listItemEl);
-}
+};
 
-buttonEl.addEventListener("click", createTaskHandler);
+// when a form is submitted, append a new list item
+
+formEl.addEventListener("submit", createTaskHandler);
 
